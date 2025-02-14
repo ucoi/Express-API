bc9e119c77b805c4e8149c38972940c53ce640a2
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-02-14 16:05:20 +0100

    added nodemon

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.json([{ name: "John Doe", age: 25 }]);
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
