3f9d7313e596f02d4352be6aa65ffed1b621c5ea
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-02-18 17:11:18 +0100

    Last Commit

--- routes/people.js
+++ routes/people.js
@@ -8,10 +8,13 @@ const {
   updatePerson,
   deletePerson,
 } = require("../controllers/people");
-router.get("/", getPeople);
-router.post("/", createPerson);
-router.post("./api/postman/people", createPersonPostman);
-router.put("/:id", updatePerson);
-router.delete("/:id", deletePerson);
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("./api/postman/people", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
+router.route("/").get(getPeople).post(createPerson);
+router.route("/api/postman/people").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 module.exports = router;
